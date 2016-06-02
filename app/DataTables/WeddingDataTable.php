1b57c708daaeb3bc19bb60dbe83c6c95af5b411d
Florindo <florindo.lopez@inmediastudio.com>
2016-06-02 17:48:56 +0200

    First commit

---
+++ app/DataTables/WeddingDataTable.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\Wedding;
+use Form;
+use Yajra\Datatables\Services\DataTable;
+
+class WeddingDataTable extends DataTable
+{
+
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function ajax()
+    {
+        return $this->datatables
+            ->eloquent($this->query())
+            ->addColumn('actions', function ($data) {
+                            return '
+                            ' . Form::open(['route' => ['weddings.destroy', $data->id], 'method' => 'delete']) . '
+                            <div class=\'btn-group\'>
+                                <a href="' . route('weddings.show', [$data->id]) . '" class=\'btn btn-default btn-xs\'><i class="glyphicon glyphicon-eye-open"></i></a>
+                                <a href="' . route('weddings.edit', [$data->id]) . '" class=\'btn btn-default btn-xs\'><i class="glyphicon glyphicon-edit"></i></a>
+                                ' . Form::button('<i class="glyphicon glyphicon-trash"></i>', [
+                                'type' => 'submit',
+                                'class' => 'btn btn-danger btn-xs',
+                                'onclick' => "return confirm('Are you sure?')"
+                            ]) . '
+                            </div>
+                            ' . Form::close() . '
+                            ';
+                        })
+            ->make(true);
+    }
+
+    /**
+     * Get the query object to be processed by datatables.
+     *
+     * @return \Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder
+     */
+    public function query()
+    {
+        $weddings = Wedding::query();
+        
+        return $this->applyScopes($weddings);
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\Datatables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+            ->columns(array_merge(
+                $this->getColumns(),
+                [
+                    'actions' => [
+                        'orderable' => false,
+                        'searchable' => false,
+                        'printable' => false,
+                        'exportable' => false
+                    ]
+                ]
+            ))
+            ->parameters([
+                'dom' => 'Bfrtip',
+                'scrollX' => true,
+                'buttons' => [
+                    'csv',
+                    'excel',
+                    'pdf',
+                    'print',
+                    'reset',
+                    'reload'
+                ],
+            ]);
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    private function getColumns()
+    {
+        return [
+            'name' => ['name' => 'name', 'data' => 'name'],
+            'id_user' => ['name' => 'id_user', 'data' => 'id_user'],
+            'id_type' => ['name' => 'id_type', 'data' => 'id_type']
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'weddings';
+    }
+}
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,4 @@
+<li class="{{ Request::is('weddings*') ? 'active' : '' }}">
+    <a href="{!! route('weddings.index') !!}"><i class="fa fa-edit"></i><span>Weddings</span></a>
+</li>
+
